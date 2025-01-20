6edbf559d73755eaade99da5f6158b42e57b2601
Alvaro R Martin <83295669+exagonsoft@users.noreply.github.com>
2025-01-20 19:10:35 +0100

    #45 Samre thing

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
@@ -10,5 +11,6 @@ import { AuthModule } from './auth/auth.module';
     MongooseModule.forRoot(process.env.MONGO_URI),
     AuthModule,
   ],
+  controllers:[AppController]
 })
 export class AppModule {}
