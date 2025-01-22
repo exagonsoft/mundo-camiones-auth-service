5b5e92580cb89a8e526d5370d0e962dfb633f5f0
Alvaro R Martin <83295669+exagonsoft@users.noreply.github.com>
2025-01-22 05:10:23 +0100

    #45 Update white list

--- src/settings/corsWhitelist.ts
+++ src/settings/corsWhitelist.ts
@@ -1,2 +1,2 @@
 /* eslint-disable prettier/prettier */
-export const allowedDomains = ['http://localhost:3000/', 'https://subastas.client.martinnotaryfl.com/', 'https://master.d2bdppuk7eu8fy.amplifyapp.com/'];
+export const allowedDomains = ['http://localhost:3000', 'https://subastas.client.martinnotaryfl.com', 'https://master.d2bdppuk7eu8fy.amplifyapp.com'];
